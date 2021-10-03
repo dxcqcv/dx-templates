61ede088349ad56d7e031e83ad441c8921038f38
dxcqcv <dxcqcv@gmail.com>
2021-10-03 13:47:48 +0800

    feat: finish prod env

--- webpack/v5/react/config/webpack/prod.ts
+++ webpack/v5/react/config/webpack/prod.ts
@@ -1,1 +1,55 @@
-﻿
+﻿import MiniCssExtractPlugin  from 'mini-css-extract-plugin'
+import CssMinimizerPlugin  from 'css-minimizer-webpack-plugin'
+import { merge } from 'webpack-merge'
+
+import common from './common' 
+import {  appDist } from '../paths'
+
+export default merge(common, {
+	mode:'production',
+	devtool: false,
+	output: {
+		path: appDist,
+		publicPath: '/',
+		filename: 'js/[name].[contenthash].bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(scss|css)$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 2,
+							sourceMap: false,
+							modules: true
+						}
+					},
+					'postcss-loader',
+					'sass-loader'
+				]
+			}
+		]
+	},
+	plugins:[
+		// Extracts CSS into separate files
+		new MiniCssExtractPlugin({
+			filename: 'styles/[name].[contenthash].css',
+			chunkFilename: '[id].css'
+		})
+	],
+	optimization: {
+		minimize: true,
+		minimizer: [new CssMinimizerPlugin(), '...'],
+		runtimeChunk: {
+			name: 'runtime'
+		}
+	},
+	performance: {
+		hints: false,
+		maxEntrypointSize: 512000,
+		maxAssetSize: 512000
+	}
+})
